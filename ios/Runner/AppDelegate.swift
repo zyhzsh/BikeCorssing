20db440dcebbdde7403ab92af5973c1f5f5d7d25
Shenghang Zhu <443880@student.fontys.nl>
2023-05-08 16:52:11 +0200

    Security configuration for ios platform

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,14 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyC9bn22ZcoAiJVJJAEYTziTrdkCDuV-F1I")
+    if let secretsPath = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+       let secrets = NSDictionary(contentsOfFile: secretsPath),
+       let googleMapsAPIKey = secrets["GOOGLE_MAPS_API_KEY"] as? String {
+      GMSServices.provideAPIKey(googleMapsAPIKey)
+    } else {
+      fatalError("Unable to read Google Maps API key from Secrets.plist")
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
